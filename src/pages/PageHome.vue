df3f79d0e52f4926a419ba9cd41a2c303a4b68ae
Simon Alvarado <s.alvarado@viseven.com>
2023-04-12 12:26:24 -0300

    Editing the home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,30 +1,36 @@
 <template>
   <q-page>
-      <q-input dark bottom-slots v-model="text" label="Label" counter maxlength="12" :dense="dense">
+    <div class="q-py-lg q-px-md">
+      <q-input
+        dark
+        bottom-slots
+        v-model="newQweetContent"
+        placeholder="¿Qué está pasando?"
+        counter
+        maxlength="280"
+        :dense="dense"
+      >
         <template v-slot:before>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
           </q-avatar>
-        </template>
-
-        <template v-slot:append>
-          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
-          <q-icon name="schedule" />
-        </template>
-
-        <template v-slot:hint>
-          Field hint
         </template>
 
         <template v-slot:after>
           <q-btn round dense flat icon="send" />
         </template>
       </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "PageHome",
+  data() {
+    return {
+      newQweetContent: ''
+    }
+  }
 };
 </script>
