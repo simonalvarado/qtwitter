9cb2e59eb2c982688122ee5f812c57864d0283e2
Simon Alvarado <s.alvarado@viseven.com>
2023-04-19 23:30:32 -0300

    Working on the home page, adding a new button and some other style changes

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -7,17 +7,28 @@
         v-model="newQweetContent"
         placeholder="¿Qué está pasando?"
         counter
+        autogrow
+        input-class="input-placeholder-text"
         maxlength="280"
         :dense="dense"
       >
         <template v-slot:before>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            <img
+              src="https://pbs.twimg.com/profile_images/1415466062770999296/YVthEL37_400x400.jpg"
+            />
           </q-avatar>
         </template>
 
         <template v-slot:after>
-          <q-btn round dense flat icon="send" />
+          <q-btn
+            unelevated
+            rounded
+            no-caps
+            :disable="!newQweetContent"
+            color="blue"
+            label="Twittear"
+          />
         </template>
       </q-input>
     </div>
@@ -29,8 +40,13 @@ export default {
   name: "PageHome",
   data() {
     return {
-      newQweetContent: ''
-    }
-  }
+      newQweetContent: "",
+    };
+  },
 };
 </script>
+<style>
+.input-placeholder-text::placeholder {
+  color: #fff;
+}
+</style>
