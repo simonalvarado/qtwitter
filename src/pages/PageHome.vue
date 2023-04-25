cecd7bc057271e9a3f437708b0f7c017b4f8227b
Simon Alvarado <s.alvarado@viseven.com>
2023-04-25 02:19:44 -0300

    update(PageHome): changing style on the input

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,36 +1,39 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-      <q-input
-        dark
-        bottom-slots
-        v-model="newQweetContent"
-        placeholder="¿Qué está pasando?"
-        counter
-        autogrow
-        input-class="input-placeholder-text"
-        maxlength="280"
-        :dense="dense"
-      >
-        <template v-slot:before>
-          <q-avatar>
-            <img
-              src="https://pbs.twimg.com/profile_images/1415466062770999296/YVthEL37_400x400.jpg"
-            />
-          </q-avatar>
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            unelevated
-            rounded
-            no-caps
-            :disable="!newQweetContent"
-            color="blue"
-            label="Twittear"
-          />
-        </template>
-      </q-input>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          dark
+          bottom-slots
+          v-model="newQweetContent"
+          class="new-qweet-input"
+          placeholder="¿Qué está pasando?"
+          counter
+          autogrow
+          input-class="input-placeholder-text"
+          maxlength="280"
+          :dense="dense"
+        >
+          <template v-slot:before>
+            <q-avatar>
+              <img
+                src="https://pbs.twimg.com/profile_images/1415466062770999296/YVthEL37_400x400.jpg"
+              />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col-shrink">
+        <q-btn
+          unelevated
+          rounded
+          no-caps
+          :disable="!newQweetContent"
+          class="q-mb-lg"
+          color="blue"
+          label="Twittear"
+        />
+      </div>
     </div>
   </q-page>
 </template>
@@ -48,5 +51,9 @@ export default {
 <style>
 .input-placeholder-text::placeholder {
   color: #fff;
+}
+.new-qweet-input .q-field__native {
+  font-size: 20px;
+  line-height: 24px;
 }
 </style>
